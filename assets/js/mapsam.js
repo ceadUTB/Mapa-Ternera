498a44a81496e3719bd5b9de66ea8e25be9b8a23
Andres Morelos <andresmorelos123@gmail.com>
2017-11-20 09:10:03 -0500

    MapsAm updated to 1.1.6, Map image added

--- assets/js/mapsam.js
+++ assets/js/mapsam.js
@@ -1,7 +1,8 @@
 "use strict";
-/*!Version: 1.1.4*/
+/*!Version: 1.1.6*/
 var Variables = (function () {
     function Variables() {
+        this.Locked = false;
     }
     Variables.prototype.getWH = function () {
         return this.WH_Aux;
@@ -23,6 +24,12 @@ var Variables = (function () {
     };
     Variables.prototype.getImgSRC = function () {
         return this.imgSRC;
+    };
+    Variables.prototype.setLocked = function () {
+        this.Locked = true;
+    };
+    Variables.prototype.Unlock = function () {
+        this.Locked = false;
     };
     Variables.prototype.setWH = function (Numero) {
         this.WH_Aux = Numero;
@@ -83,11 +90,11 @@ var Browser = (function () {
 }());
 var Css = (function () {
     function Css() {
-        this.Styles = ".map{ margin: 0px; padding: 0px; overflow: hidden; border: 1px #000 solid; cursor: grab; cursor: -moz-grab; cursor: -webkit-grab; background-repeat: no-repeat; } .PointVisible{ visibility: visible; animation: float 2s ease-in-out infinite; } .Visible{ visibility: visible; } .Invisible{ visibility: hidden; }  @keyframes float { 0% { box-shadow: 0 5px 15px 0px rgba(0,0,0,0.6); transform: translatey(0px); } 50% { box-shadow: 0 25px 15px 0px rgba(0,0,0,0.2); transform: translatey(-5px); } 100% { box-shadow: 0 5px 15px 0px rgba(0,0,0,0.6); transform: translatey(0px); } } .popover p{ padding: 5px; }";
+        this.Styles = ".map{ margin: 0px; padding: 0px; overflow: hidden; border: 1px #000 solid; cursor: grab; cursor: -moz-grab; cursor: -webkit-grab; background-repeat: no-repeat; } .PointVisible{ visibility: visible; animation: float 2s ease-in-out infinite; } .Visible{ visibility: visible; } .Invisible{ visibility: hidden; }  @keyframes float { 0% { box-shadow: 0 5px 15px 0px rgba(0,0,0,0.6); transform: translatey(0px); } 50% { box-shadow: 0 25px 15px 0px rgba(0,0,0,0.2); transform: translatey(-5px); } 100% { box-shadow: 0 5px 15px 0px rgba(0,0,0,0.6); transform: translatey(0px); } } .popover p{ padding: 5px; } .popover h4 { text-align:center; } .ac-container{width: 250px;margin: 10px auto 30px auto;} .ac-container label{font-family: 'BebasNeueRegular', 'Arial Narrow', Arial, sans-serif;padding: 5px 20px;position: relative;z-index: 20;display: block;height: 30px;cursor: pointer;color: #777;text-shadow: 1px 1px 1px rgba(255,255,255,0.8);line-height: 33px;font-size: 19px;background: linear-gradient(top, #ffffff 1%,#eaeaea 100%);box-shadow:0px 0px 0px 1px rgba(155,155,155,0.3),1px 0px 0px 0px rgba(255,255,255,0.9) inset,0px 2px 2px rgba(0,0,0,0.1);} .ac-container label:hover{background: #fff;} .ac-container input:checked + label,.ac-container input:checked + label:hover{background: #c6e1ec;color: #3d7489;text-shadow: 0px 1px 1px rgba(255,255,255, 0.6);box-shadow:0px 0px 0px 1px rgba(155,155,155,0.3),0px 2px 2px rgba(0,0,0,0.1);} .ac-container label:hover:after,.ac-container input:checked + label:hover:after{content: '';position: absolute;width: 24px;height: 24px;right: 13px;top: 7px;} .ac-container input:checked + label:hover:after{} .ac-container input{display: none;} .ac-container article{background: rgba(255, 255, 255, 0.5);margin-top: -1px;overflow: hidden;height: 0px;position: relative;z-index: 10;transition:height 0.3s ease-in-out,box-shadow 0.6s linear;}.ac-container input:checked ~ article{transition:height 0.5s ease-in-out,box-shadow 0.1s linear;box-shadow: 0px 0px 0px 1px rgba(155,155,155,0.3);} .ac-container article p{font-style: italic;color: #777;line-height: 23px;font-size: 14px;padding: 10px;text-shadow: 1px 1px 1px rgba(255,255,255,0.8);} .ac-container input:checked ~ article.ac-medium{height: 360px;} .ac-container article img{max-width:250px; max-heigth:150px}";
     }
     Css.prototype.Config = function (StylesConfig) {
         var StylePoint = ".Point{ border-radius: 100%;  cursor: pointer; position: absolute; ";
-        var StylePopOver = ".popover{ border-radius: 5%; position: absolute; width: 250px; ";
+        var StylePopOver = ".popover{ position: absolute; width: 250px; -webkit-box-shadow: 0px 0px 12px 0px rgba(0,0,0,0.75);-moz-box-shadow: 0px 0px 12px 0px rgba(0,0,0,0.75);box-shadow: 0px 0px 12px 0px rgba(0,0,0,0.75); ";
         if (StylesConfig.Punto) {
             StylesConfig.Punto.background_color ? StylePoint += "background-color: " + StylesConfig.Punto.background_color + ";" : StylePoint += "background-color: #fff;";
             StylesConfig.Punto.border_color ? StylePoint += "border: 2px solid " + StylesConfig.Punto.border_color + ";" : StylePoint += "border: 2px solid #000;";
@@ -131,8 +138,14 @@ var Mapa = (function () {
             Var.getMap().style.cursor = _this.bw.gotCursorName()[1];
             _this.X_ini = event_clic.layerX;
             _this.Y_ini = event_clic.layerY;
+            Var.Unlock();
             Var.getMap().onmousemove = function (evento_move) {
-                if (evento_move.buttons === 1) {
+                if (evento_move.buttons === 1 && !Var.Locked) {
+                    for (var i = 0; i < document.getElementsByClassName("popover").length; i++) {
+                        Var.getMap().getElementsByClassName("popover")[i].classList.remove("Visible");
+                        Var.getMap().getElementsByClassName("popover")[i].classList.remove("Invisible");
+                        Var.getMap().getElementsByClassName("popover")[i].classList.add("Invisible");
+                    }
                     _this.moveBackground(_this.getCoodinates(evento_move.layerX, evento_move.layerY));
                     _this.updateCoordenateMax();
                     for (var _i = 0, Puntos_1 = Puntos; _i < Puntos_1.length; _i++) {
@@ -173,17 +186,32 @@ var Mapa = (function () {
     return Mapa;
 }());
 var PopOver = (function () {
-    function PopOver(Var, texto) {
+    function PopOver(Var, config) {
         this.coordenadas = [0, 0];
         this.Var = Var;
-        this.texto = texto;
+        config.titulo ? this.Titulo = config.titulo : false;
+        config.acordeonItems ? this.items = config.acordeonItems : false;
         this.Elemento_Div = document.createElement("div");
-        this.Elemento_P = document.createElement("p");
-        var Texto = document.createTextNode(this.texto);
+        this.Elemento_P = document.createElement("h4");
+        var Texto = document.createTextNode(this.Titulo);
+        var hr = document.createElement("hr");
         this.Elemento_P.appendChild(Texto);
         this.Elemento_Div.appendChild(this.Elemento_P);
+        this.Elemento_Div.appendChild(hr);
+        this.Section = document.createElement("section");
+        this.Section.classList.add("ac-container");
+        this.Elemento_Div.appendChild(this.Section);
+        this.Elemento_Div.style.height = Var.getHG() + "px";
         this.Elemento_Div.classList.add("Invisible");
         this.Elemento_Div.classList.add("popover");
+        this.makeItems();
+        this.Elemento_Div.style.cursor = "auto";
+        this.Elemento_Div.onmouseup = function () {
+            Var.setLocked();
+        };
+        this.Elemento_Div.onmousemove = function () {
+            Var.setLocked();
+        };
         Var.getMap().appendChild(this.Elemento_Div);
     }
     PopOver.prototype.setCordenadas = function (coordenadas) {
@@ -191,6 +219,34 @@ var PopOver = (function () {
     };
     PopOver.prototype.getCordenadas = function () {
         return this.coordenadas;
+    };
+    PopOver.prototype.makeItems = function () {
+        var i = 1;
+        for (var _i = 0, _a = this.items; _i < _a.length; _i++) {
+            var item = _a[_i];
+            var div = document.createElement("div");
+            var input = document.createElement("input");
+            input.type = "radio";
+            input.id = "ac-" + i + "-" + item.titulo;
+            input.name = "accordion-1";
+            var label = document.createElement("label");
+            item.titulo ? label.htmlFor = "ac-" + i + "-" + item.titulo : false;
+            item.titulo ? label.appendChild(document.createTextNode(item.titulo)) : false;
+            var article = document.createElement("article");
+            article.classList.add("ac-medium");
+            var image = document.createElement("img");
+            var p = document.createElement("p");
+            item.imgSRC ? image.src = item.imgSRC : false;
+            item.imgSRC ? article.appendChild(image) : false;
+            item.imgSRC ? article.appendChild(document.createElement("br")) : false;
+            item.descripcion ? p.appendChild(document.createTextNode(item.descripcion)) : false;
+            item.descripcion ? article.appendChild(p) : false;
+            div.appendChild(input);
+            div.appendChild(label);
+            div.appendChild(article);
+            this.Section.appendChild(div);
+            i = i + 1;
+        }
     };
     PopOver.prototype.showUp = function () {
         this.Repositionate();
@@ -202,8 +258,7 @@ var PopOver = (function () {
         this.Elemento_Div.classList.remove("Visible");
     };
     PopOver.prototype.Repositionate = function () {
-        (this.Var.getWH() - this.coordenadas[0]) < 300 ? this.Elemento_Div.style.left = (this.coordenadas[0] - 260) + "px" : this.Elemento_Div.style.left = (this.coordenadas[0] + 50) + "px";
-        this.Elemento_Div.style.top = this.coordenadas[1] + "px";
+        (this.Var.getWH() - this.coordenadas[0]) < 300 ? this.Elemento_Div.style.left = "9px" : this.Elemento_Div.style.left = (this.Var.getWH() - 241) + "px";
     };
     return PopOver;
 }());
@@ -217,11 +272,8 @@ var Punto = (function () {
         this.Elemento.classList.add("Point");
         this.Elemento.classList.add("Invisible");
         Var.getMap().appendChild(this.Elemento);
-        this.Elemento.onmouseenter = function (event) {
+        this.Elemento.onclick = function (event) {
             _this.showPopOver();
-        };
-        this.Elemento.onmouseleave = function (event) {
-            _this.disappearPopOver();
         };
     }
     Punto.prototype.CanIBeVisible = function () {
@@ -289,13 +341,13 @@ var mapsAM = (function () {
     mapsAM.prototype.SayError = function (Mensaje) {
         throw new TypeError(Mensaje);
     };
-    mapsAM.prototype.createNewPoint = function (Coordenadas, TextoPopOver) {
+    mapsAM.prototype.createNewPoint = function (Coordenadas, ConfigPopOver) {
         var aux = new Punto(this.Var, Coordenadas);
-        aux.setPopOver(this.createNewPopOver(TextoPopOver));
+        aux.setPopOver(this.createNewPopOver(ConfigPopOver));
         this.Puntos.push(aux);
     };
-    mapsAM.prototype.createNewPopOver = function (TextoPopOver) {
-        return new PopOver(this.Var, TextoPopOver);
+    mapsAM.prototype.createNewPopOver = function (ConfigPopOver) {
+        return new PopOver(this.Var, ConfigPopOver);
     };
     return mapsAM;
 }());
